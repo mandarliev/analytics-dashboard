6a8ca1c56afb04e07395015ffb60bf47f00d6553
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-16 13:58:04 +0200

    Initial commit

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return(
+        <p>Hello world</p>
+    )
+}
+
+export default Page;
---
+++ src/utils/analytics.ts
@@ -0,0 +1,26 @@
+import { redis } from "@/lib/redis";
+
+type AnalyticsArgs = {
+    retention?: number
+}
+
+export class Analytics {
+    private retention: number = 60 * 60 * 24 * 7;
+
+    constructor(opts?: AnalyticsArgs) {
+        if (opts?.retention) {
+            this.retention = opts.retention
+        }
+    }
+
+    async track(namespace: string, event: object = {}) {
+        
+        const key = `analytics::${namespace}`
+        
+        // db call to persist this event
+        await redis.hincrby(key, JSON.stringify(event), 1)
+
+    }
+}
+
+export const analytics = new Analytics({retention: 3600})
